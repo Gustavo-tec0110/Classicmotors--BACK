1a921ad4a10f215ba6f87c927a17e3a848780f58
Gustavo <gustavo.sl.0110@gmail.com>
2026-01-12 21:03:03 -0300

    remove scripts de seed e limpeza do banco

--- limparcarros.js
+++
@@ -1,8 +0,0 @@
-const db = require("./src/database/db.js");
-
-db.run("DELETE FROM carros", () => {
-    db.run("DELETE FROM sqlite_sequence WHERE name='carros'", () => {
-        console.log("Tabela limpa e IDs resetados.");
-        db.close();
-    });
-});
